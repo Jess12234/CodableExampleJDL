5a296e12d992586705c21ba4b352f331784732fb
Jessica Lowry <jdl14a@dmc-207-04.local>
2019-03-08 08:32:45 -0600

    Did the thing with the video

--- Codable Example/ViewController.swift
+++ Codable Example/ViewController.swift
@@ -8,12 +8,38 @@
 
 import UIKit
 
+struct Country: Decodable{
+    let name: String
+    let capital: String
+    let region: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //changed things
+        
+        let url = "https://restcountries.eu/rest/v2/all"
+        
+        let urlObj = URL(string: url)
+        
+        URLSession.shared.dataTask(with: urlObj!)
+        {
+            (data, response, error) in
+            
+            do {
+                var countires = try JSONDecoder().decode([Country].self, from: data!)
+                
+                for country in countires{
+                    print(country.name + ": " + country.capital)
+                }
+            } catch{
+                print("We got an error")
+            }
+            
+        }
     }
 
 
